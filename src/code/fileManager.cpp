a121a9c5bad3c99e545f2c530b1761a6d0ef1879
Martin Santos <martin.n.santos@gmail.com>
2017-04-13 17:19:55 -0300

    agregando test3

--- src/code/fileManager.cpp
+++ src/code/fileManager.cpp
@@ -29,59 +29,32 @@ Parametros FileManager::read(){
 		inputFileStream >> n >> k;
 		teams = vector<Team>(n);
 		for (int l = 0; l < k; ++l){
-			cout << "read 0";
 			string date;
-			cout << "read 1";
 			int team_i, team_j, team_i_goals, team_j_goals;
-			cout << "read 2";
-
-cout << "read 3";
 			inputFileStream >> date >> team_i >> team_i_goals >> team_j >> team_j_goals;
-			cout << "read 4";
 			//if team_i was never before seen, it is indexed in the map, so to add wins-loses-points in the correct positions.
-			cout << "read 5";
 			it = teamsMap.find(team_i);
-			cout << "read 6";
   			if (it == teamsMap.end())
-  				cout << "read 7";
 			{
-				cout << "read 8";
 				Team team_a = Team(count, team_i);
-				cout << "read 9";
 				teamsMap[team_i] = team_a;
-				cout << "read 10";
 				count++;
-				cout << "read 11";
 			}
-			cout << "read 12";
 			//if team_j was never before seen, it is indexed in the map, so to add wins-loses-points in the correct positions.
-			cout << "read 13";
 			it = teamsMap.find(team_j);
-			cout << "read 14";
   			if (it == teamsMap.end())
-  				cout << "read 15";
 			{
-				cout << "read 16";
 				Team team_b = Team(count, team_j);
-				cout << "read 17";
 				teamsMap[team_j] = team_b;
-				cout << "read 18";
 				count++;
-				cout << "read 19";
+
 			}
-			cout << "read 20";
 			teamsMap[team_i].recordGame(team_j, team_i_goals, team_j_goals);
-			cout << "read 21";
 			//cout << teamsMap[team_i].getId() << endl;;
-			cout << "read 22";
 			teams[teamsMap[team_i].getId()] = teamsMap[team_i];
-			cout << "read 23";
 			teamsMap[team_j].recordGame(team_i, team_j_goals, team_i_goals);
-			cout << "read 24";
 			//cout << teamsMap[team_j].getId() << endl;;
-			cout << "read 25";
 			teams[teamsMap[team_j].getId()] = teamsMap[team_j];
-			cout << "read 26";
 		}
 
 		
